c5eacc6005860ff98a8d7b354fafb4b0e266e2d8
peymansheybani <peyman.sheybanii@gmail.com>
2019-01-20 14:16:55 +0330

    first commit

---
+++ src/Bale.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peyman
+ * Date: 20/01/19
+ * Time: 14:02
+ */
+
+namespace Vicente\BALE;
+
+
+class Bale {
+
+	private $token = "";
+	public function __construct($token) {
+		$this->token = $token;
+	}
+
+	public function hears() {
+
+	}
+}
